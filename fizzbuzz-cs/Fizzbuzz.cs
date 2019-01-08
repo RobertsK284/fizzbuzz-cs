e6104b1aa05ef3f0605f81876dca387ba735db53
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2019-01-08 14:19:22 +0000

    fizzbuzz code and test

---
+++ fizzbuzz-cs/Fizzbuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace fizzbuzz_csharp
+{
+    public class Fizzbuzz
+    {
+        public string Play(int number)
+        {
+
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 100;
+            Fizzbuzz n = new Fizzbuzz();
+
+            for (int i = a; i < b + 1; i = i + 1)
+            {
+                Console.WriteLine(n.Play(i));
+            }
+        }
+    }
+}
---
+++ fizzbuzz-cs/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using fizzbuzz_csharp;
+
+namespace fizzbuzztest_csharp
+{
+    [TestFixture()]
+    public class FizzbuzzTest
+    {
+        [Test()]
+        public void Play_Returns_Fizzbuzz_Fizz_Buzz()
+        {
+            // arrange
+            Fizzbuzz fizzbuzz = new Fizzbuzz();
+            int c = 9;
+            string expected = "Fizz";
+
+            // act
+
+            string actual = fizzbuzz.Play(c);
+
+            // assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
+
+
