4d81a9aca4d29ef979c5c9d8e950a0dacb9aab3d
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2019-01-08 16:32:18 +0000

    more tests added

--- fizzbuzz-cs/Test.cs
+++ fizzbuzz-cs/Test.cs
@@ -8,7 +8,7 @@ namespace fizzbuzztest_csharp
     public class FizzbuzzTest
     {
         [Test()]
-        public void Play_Returns_Fizzbuzz_Fizz_Buzz()
+        public void Play_Returns_Fizz()
         {
             // arrange
             Fizzbuzz fizzbuzz = new Fizzbuzz();
@@ -16,14 +16,41 @@ namespace fizzbuzztest_csharp
             string expected = "Fizz";
 
             // act
-
             string actual = fizzbuzz.Play(c);
 
             // assert
-
             Assert.AreEqual(expected, actual);
         }
 
+        [Test()]
+        public void Play_Returns_FizzBuzz()
+        {
+            // arrange
+            Fizzbuzz fizzbuzz = new Fizzbuzz();
+            int d = 15;
+            string expected = "FizzBuzz";
+
+            // act
+            string actual = fizzbuzz.Play(d);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test()]
+        public void Play_Returns_Buzz()
+        {
+            // arrange
+            Fizzbuzz fizzbuzz = new Fizzbuzz();
+            int e = 10;
+            string expected = "Buzz";
+
+            // act
+            string actual = fizzbuzz.Play(e);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
 
